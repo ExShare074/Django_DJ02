a8c2e01bdd2a875c7a2127f140b1c4930c4a4c12
Mihail <168080573+ExShare074@users.noreply.github.com>
2025-05-11 23:38:24 +0300

    add new code

--- zerocoder/zerocoder/urls.py
+++ zerocoder/zerocoder/urls.py
@@ -5,9 +5,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # 1) Сначала всё, что приходит на /data/ и /test/
-    path('', include('data_app.urls')),
+    # всё, что на /data/... → data_app
+    path('data/', include('data_app.urls')),
 
-    # 2) И только после этого — «пустой» путь для главной
+    # всё, что на /new_post/, /new и / – → main
     path('', include('main.urls')),
 ]
