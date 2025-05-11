06882f1d1673de9f2ae012872440d8478b70ff60
Mihail <168080573+ExShare074@users.noreply.github.com>
2025-05-11 23:20:13 +0300

    add new code

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
         path('', views.index),
+        path('new_post/', views.new_post, name='new_post'),
         path('new', views.new),
         path('', views.index_view, name='index')
 ]
--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -4,8 +4,12 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html')
 
+def new_post(request):
+    return render(request, 'main/new_post.html')
+
 def new(request):
     return HttpResponse('<h1>Это вторая страница проекта на django</h1>')
 
 def index_view(request):
     return render(request, 'main/index.html')
+
