d80c892760c679e5f0c5e14776eb074102e26d3e
Mihail <168080573+ExShare074@users.noreply.github.com>
2025-05-23 16:02:45 +0300

    Реорганизованы маршруты: main на '', data_app на 'data/'

--- ZeroCoder/ZeroCoder/urls.py
+++ ZeroCoder/ZeroCoder/urls.py
@@ -1,4 +1,3 @@
-# ZeroCoder/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -7,9 +6,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # теперь подключаем data_app без префикса
-    path('', include('data_app.urls')),
-
-    # main остаётся с теми же маршрутами
+    # main подключаем первым без префикса
     path('', include('main.urls')),
+
+    # data_app теперь с префиксом data/
+    path('data/', include('data_app.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
